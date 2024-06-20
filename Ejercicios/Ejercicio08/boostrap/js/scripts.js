2831f3401cf9e186dcbc08e131a71098d928809a
Ariel <arielplante02@gmail.com>
2024-06-19 21:41:14 -0300

    Se agregaron nuevos archivos

---
+++ Ejercicios/Ejercicio08/boostrap/js/scripts.js
@@ -0,0 +1,17 @@
+// scripts.js
+
+// Puedes agregar scripts personalizados aquí
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Ejemplo de script: Mostrar alerta al cargar la página
+    console.log("Página cargada correctamente");
+  });
+
+document.addEventListener("click", function() {
+    console.log("Hicistes click");
+  });
+
+document.addEventListener("scroll", function() {
+    console.log("Scrolleastes");
+  });  
+  
